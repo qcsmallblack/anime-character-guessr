66c057187cebd5df81428e928e77bedf5286bd95
Kenny Li <limz000609@gmail.com>
2025-03-28 20:13:50 -0500

    scrollbar

--- client_v2/src/components/SocialLinks.jsx
+++ client_v2/src/components/SocialLinks.jsx
@@ -16,7 +16,7 @@ function SocialLinks({ onSettingsClick, onHelpClick }) {
       <a href="https://bangumi.tv/" target="_blank" rel="noopener noreferrer" className="social-link">
         <img src="https://avatars.githubusercontent.com/u/7521082?s=200&v=4" alt="Bangumi" className="bangumi-icon" />
       </a>
-      <a href="https://github.com/kennylimz/anime-character-guessr" target="_blank" rel="noopener noreferrer" className="social-link">
+      <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="social-link">
         <i className="fab fa-github"></i>
       </a>
     </div>
