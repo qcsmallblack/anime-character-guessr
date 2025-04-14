73c674ad97f8ad251e7e9e1f39293716e14bcd8d
Kenny Li <limz000609@gmail.com>
2025-04-14 07:34:23 -0500

    Update Home.jsx

--- client_v3/src/pages/Home.jsx
+++ client_v3/src/pages/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
           灵感来源<a href="https://blast.tv/counter-strikle">BLAST.tv</a>,
           数据来源<a href="https://bgm.tv/">Bangumi</a>。<br/>
           <a href="https://space.bilibili.com/87983557">作者</a>：“感恩Bangumi。为了减少服务器负担，请大家错峰游玩。”<br/>
-          如果一直初始化错误请不要重复尝试……
+          如果一直初始化错误请不要重复尝试……就是被限流了。
         </p>
         {/* <p>
           <a href="https://space.bilibili.com/87983557">作者</a>：实在对不起了大家，详情见……没有详情了，代码里调用的上游API挂了，所以暂时不能玩了。大伙早点休息吧。
