34eca11035cc402f7796a6797b54b9dc42ede9ed
EST-NINE <143050853+EST-NINE@users.noreply.github.com>
2025-04-16 19:45:07 +0800

    chore: 恢复上线 (#11, #12)

--- client_v3/src/pages/Home.jsx
+++ client_v3/src/pages/Home.jsx
@@ -18,7 +18,6 @@ const Home = () => {
       <div className="game-modes">
         <Link to="/singleplayer" className="mode-button">
           <h2>单人</h2>
-          <small>并非不会卡😅</small>
         </Link>
         <Link to="/multiplayer" className="mode-button">
           <h2>多人</h2>
@@ -31,9 +30,8 @@ const Home = () => {
           建议使用桌面端浏览器游玩。
           <br/>
           灵感来源<a href="https://blast.tv/counter-strikle">BLAST.tv</a>,
-          数据来源<a href="https://bgm.tv/">Bangumi</a>。<br/>
-          <a href="https://space.bilibili.com/87983557">作者</a>：“实在对不起了大家，详情见……没有详情了，由于我制作网站时缺乏考虑，给bangumi带来了很大的负担。<br/>
-          他们的管理员也很头大，所以我会暂时关闭网站，直到完成优化方案。感谢大家这段时间的热情和支持。”
+          数据来源<a href="https://bgm.tv/">Bangumi</a>。<br />
+          <a href="https://space.bilibili.com/87983557">作者</a>：“感谢 Bangumi 管理员的优化支持。感谢大家这段时间的热情和支持。”
         </p>
       </div>
     </div>
