7ee40281f44849323c0b6bdefb3788e921e93e27
Kenny Li <limz000609@gmail.com>
2025-04-18 10:14:31 -0500

    Update anime.js

--- client_v3/src/utils/anime.js
+++ client_v3/src/utils/anime.js
@@ -545,9 +545,9 @@ function generateFeedback(guess, answerCharacter) {
     if (yearDiff === 0) {
       yearFeedback = '=';
     } else if (yearDiff > 0) {
-      yearFeedback = yearDiff <= 1 ? '+' : '++';
+      yearFeedback = yearDiff <= 2 ? '+' : '++';
     } else {
-      yearFeedback = yearDiff >= -1 ? '-' : '--';
+      yearFeedback = yearDiff >= -2 ? '-' : '--';
     }
     result.earliestAppearance = {
       guess: guess.earliestAppearance,
