98ba5a95d8d06aba06c74d579913f990c2223b1f
Kenny Li <limz000609@gmail.com>
2025-04-19 06:12:45 -0500

    Update Home.jsx

--- client_v3/src/pages/Home.jsx
+++ client_v3/src/pages/Home.jsx
@@ -31,9 +31,10 @@ const Home = () => {
           <br/>
           灵感来源<a href="https://blast.tv/counter-strikle"> BLAST.tv </a>,
           数据来源<a href="https://bgm.tv/"> Bangumi </a>。<br />
-          <a href="https://space.bilibili.com/87983557">@作者</a>：“感谢 Bangumi 管理员的优化支持，以及各位网友贡献的代码。感谢大家这段时间的热情和支持。<br/>
-          角色标签暂时用了大佬分享的爬虫数据，之后会换成大家贡献的标签的。祝大家玩得开心！”<br/>
-          作品标签逻辑已重写。更新了老二次元友好的设置项。
+          <a href="https://space.bilibili.com/87983557">@作者</a>：“感谢 Bangumi 管理员的优化支持，以及各位网友贡献的代码。感谢大家这段时间的热情和支持。”<br/>
+          角色标签有大佬帮忙持续更新，之后会加入点赞倒赞的页面。<br/>
+          作品标签逻辑已重写。更新了老二次元友好的设置项。<br/>
+          有好的预设或目录可以B站私信<a href="https://space.bilibili.com/87983557">作者</a>，可能被采纳成为每日推荐或者常驻预设！
         </p>
       </div>
     </div>
